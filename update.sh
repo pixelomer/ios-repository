c4bc43c5d07c9b45da5fde99933a1b325cce79bd
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-03-10 19:16:28 +0300

    Initial commit

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+md5() {
+  command md5 -q "${@}"
+}
+
+sha256() {
+  command shasum -a 256 "${@}" | awk '{ print $1 }'
+}
+
+len() {
+  command wc -c "${@}" | awk '{ print $1 }'
+}
+
+entry() {
+  printf "$("$1" "$2") $(len "$2") $2"
+}
+
+dpkg-scanpackages -t deb -m debs | tee Packages | bzip2 -c > Packages.bz2
+cat > Release <<EOF
+$(cat Release.template)
+MD5Sum:
+  $(entry md5 Packages)
+  $(entry md5 Packages.bz2)
+SHA256Sum:
+  $(entry sha256 Packages)
+  $(entry sha256 Packages.bz2)
+EOF
